effad0c36a15cc5422b7995074bad2b24e20324c
Go技术栈学习 <70687489+916542650@users.noreply.github.com>
2021-01-25 16:47:10 +0800

    Create 036.go

---
+++ 036-两个链表的第一个公共结点/036.go
@@ -0,0 +1,16 @@
+func FindFirstCommonNode( pHead1 *ListNode ,  pHead2 *ListNode ) *ListNode {
+    // write code here
+	m := make(map[*ListNode]bool)
+	for pHead1 != nil {
+		m[pHead1] = true
+		pHead1 = pHead1.Next
+	}
+
+	for pHead2 != nil {
+		if _, ok := m[pHead2]; ok {
+			return pHead2
+		}
+		pHead2 = pHead2.Next
+	}
+	return pHead2
+}
